3700758a9b4c153b855db09db401560d070e12b9
alexadrake <alexa@speakeasyapi.dev>
2023-02-10 15:34:37 -0800

    wrap in function

--- src/transport.ts
+++ src/transport.ts
@@ -1,142 +1,135 @@
 import * as grpc from "@grpc/grpc-js";
+import {CallOptions, Client, ClientUnaryCall, Metadata, ServiceError} from "@grpc/grpc-js";
 
+import {EmbedAccessTokenRequest} from "@speakeasy-api/speakeasy-schemas/registry/embedaccesstoken/embedaccesstoken_pb";
 import {
-  CallOptions,
-  Client,
-  ClientUnaryCall,
-  Metadata,
-  ServiceError,
-} from "@grpc/grpc-js";
-
-import { EmbedAccessTokenRequest } from "@speakeasy-api/speakeasy-schemas/registry/embedaccesstoken/embedaccesstoken_pb";
-import { EmbedAccessTokenServiceClient } from "@speakeasy-api/speakeasy-schemas/registry/embedaccesstoken/embedaccesstoken_grpc_pb";
-import { IngestRequest } from "@speakeasy-api/speakeasy-schemas/registry/ingest/ingest_pb";
-import { IngestServiceClient } from "@speakeasy-api/speakeasy-schemas/registry/ingest/ingest_grpc_pb";
-import { Message } from "google-protobuf";
+  EmbedAccessTokenServiceClient
+} from "@speakeasy-api/speakeasy-schemas/registry/embedaccesstoken/embedaccesstoken_grpc_pb";
+import {IngestRequest} from "@speakeasy-api/speakeasy-schemas/registry/ingest/ingest_pb";
+import {IngestServiceClient} from "@speakeasy-api/speakeasy-schemas/registry/ingest/ingest_grpc_pb";
+import {Message} from "google-protobuf";
 
 const grpcDeadline = () => getRPCDeadline(1000); // 1 Second deadline
 
 export class GRPCClient {
-  private ingestClient: Promisified<IngestServiceClient>;
-  private embedClient: Promisified<EmbedAccessTokenServiceClient>;
-  private apiKey: string;
-  private apiID: string;
-  private versionID: string;
+    private ingestClient: Promisified<IngestServiceClient>;
+    private embedClient: Promisified<EmbedAccessTokenServiceClient>;
+    private apiKey: string;
+    private apiID: string;
+    private versionID: string;
 
-  public constructor(
-    address: string,
-    apiKey: string,
-    apiID: string,
-    versionID: string,
-    secure: boolean
-  ) {
-    var credentials = grpc.credentials.createSsl();
-    if (!secure) {
-      credentials = grpc.credentials.createInsecure();
+    public constructor(
+        address: string,
+        apiKey: string,
+        apiID: string,
+        versionID: string,
+        secure: boolean
+    ) {
+        var credentials = grpc.credentials.createSsl();
+        if (!secure) {
+            credentials = grpc.credentials.createInsecure();
+        }
+
+        this.ingestClient = promisify(
+            new IngestServiceClient(address, credentials)
+        );
+        this.embedClient = promisify(
+            new EmbedAccessTokenServiceClient(address, credentials)
+        );
+
+        this.apiKey = apiKey;
+        this.apiID = apiID;
+        this.versionID = versionID;
     }
 
-    this.ingestClient = promisify(
-      new IngestServiceClient(address, credentials)
-    );
-    this.embedClient = promisify(
-      new EmbedAccessTokenServiceClient(address, credentials)
-    );
+    public send(har: string, pathHint: string, customerID: string) {
+        const metadata = new grpc.Metadata();
+        metadata.set("x-api-key", this.apiKey);
 
-    this.apiKey = apiKey;
-    this.apiID = apiID;
-    this.versionID = versionID;
-  }
+        const request = new IngestRequest();
+        request.setApiId(this.apiID);
+        request.setVersionId(this.versionID);
+        request.setHar(har);
+        request.setPathHint(pathHint);
+        request.setCustomerId(customerID);
 
-  public send(har: string, pathHint: string, customerID: string) {
-    const metadata = new grpc.Metadata();
-    metadata.set("x-api-key", this.apiKey);
+        this.ingestClient
+            .ingest(request, metadata, {deadline: grpcDeadline()})
+            .catch((err) => {
+                if (err) {
+                    console.error(err); // TODO log error with a provided logger?
+                }
+            });
+    }
 
-    const request = new IngestRequest();
-    request.setApiId(this.apiID);
-    request.setVersionId(this.versionID);
-    request.setHar(har);
-    request.setPathHint(pathHint);
-    request.setCustomerId(customerID);
+    public async getEmbedAccessToken(
+        req: EmbedAccessTokenRequest
+    ): Promise<string> {
+        const metadata = new grpc.Metadata();
+        metadata.set("x-api-key", this.apiKey);
 
-    this.ingestClient
-      .ingest(request, metadata, { deadline: grpcDeadline() })
-      .catch((err) => {
-        if (err) {
-          console.error(err); // TODO log error with a provided logger?
-        }
-      });
-  }
-
-  public async getEmbedAccessToken(
-    req: EmbedAccessTokenRequest
-  ): Promise<string> {
-    const metadata = new grpc.Metadata();
-    metadata.set("x-api-key", this.apiKey);
-
-    const res = await this.embedClient.get(req, metadata);
-    return res.getAccesstoken();
-  }
+        const res = await this.embedClient.get(req, metadata);
+        return res.getAccessToken();
+    }
 }
 
 function getRPCDeadline(deadline: number): Date {
-  return new Date(Date.now() + deadline);
+    return new Date(Date.now() + deadline);
 }
 
 type OriginalCall<T, U> = (
-  request: T,
-  metadata: Metadata,
-  options: Partial<CallOptions>,
-  callback: (error: ServiceError, res: U) => void
+    request: T,
+    metadata: Metadata,
+    options: Partial<CallOptions>,
+    callback: (error: ServiceError, res: U) => void
 ) => ClientUnaryCall;
 
 type PromisifiedCall<T, U> = (
-  request: T,
-  metadata?: Metadata,
-  options?: Partial<CallOptions>
+    request: T,
+    metadata?: Metadata,
+    options?: Partial<CallOptions>
 ) => Promise<U>;
 
 export type Promisified<C> = { $: C } & {
-  [prop in Exclude<keyof C, keyof Client>]: C[prop] extends OriginalCall<
-    infer T,
-    infer U
-  >
-    ? PromisifiedCall<T, U>
-    : never;
+    [prop in Exclude<keyof C, keyof Client>]: C[prop] extends OriginalCall<infer T,
+            infer U>
+        ? PromisifiedCall<T, U>
+        : never;
 };
 
 export function promisify<C extends Client>(client: C): Promisified<C> {
-  return new Proxy(client, {
-    get: (target, descriptor) => {
-      let stack = "";
+    return new Proxy(client, {
+        get: (target, descriptor) => {
+            let stack = "";
 
-      // this step is required to get the correct stack trace
-      // of course, this has some performance impact, but it's not that big in comparison with grpc calls
-      try {
-        throw new Error();
-      } catch (e) {
-        stack = e.stack;
-      }
+            // this step is required to get the correct stack trace
+            // of course, this has some performance impact, but it's not that big in comparison with grpc calls
+            try {
+                throw new Error();
+            } catch (e) {
+                stack = e.stack;
+            }
 
-      if (descriptor === "$") {
-        return target;
-      }
+            if (descriptor === "$") {
+                return target;
+            }
 
-      return (...args: any[]) =>
-        new Promise((resolve, reject) =>
-          target[descriptor](
-            ...[
-              ...args,
-              (err: ServiceError, res: Message) => {
-                if (err) {
-                  err.stack += stack;
-                  reject(err);
-                } else {
-                  resolve(res);
-                }
-              },
-            ]
-          )
-        );
-    },
-  }) as unknown as Promisified<C>;
+            return (...args: any[]) =>
+                new Promise((resolve, reject) =>
+                    target[descriptor](
+                        ...[
+                            ...args,
+                            (err: ServiceError, res: Message) => {
+                                if (err) {
+                                    err.stack += stack;
+                                    reject(err);
+                                } else {
+                                    resolve(res);
+                                }
+                            },
+                        ]
+                    )
+                );
+        },
+    }) as unknown as Promisified<C>;
 }
